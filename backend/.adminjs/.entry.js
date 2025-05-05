081ee0bb6bb934580d23b579402c33c37a26b16d
Timur Salabaev <yosanti@Timurs-MacBook-Air.local>
2025-05-05 16:38:13 +0300

    hardcode and last try 2

--- backend/.adminjs/.entry.js
+++
@@ -1,9 +0,0 @@
-AdminJS.UserComponents = {}
-import ImageUploadEditComponent from '../admin-components/ImageUploadEditComponent'
-AdminJS.UserComponents.ImageUploadEditComponent = ImageUploadEditComponent
-import UploadEditComponent from '../node_modules/@adminjs/upload/build/features/upload-file/components/UploadEditComponent'
-AdminJS.UserComponents.UploadEditComponent = UploadEditComponent
-import UploadListComponent from '../node_modules/@adminjs/upload/build/features/upload-file/components/UploadListComponent'
-AdminJS.UserComponents.UploadListComponent = UploadListComponent
-import UploadShowComponent from '../node_modules/@adminjs/upload/build/features/upload-file/components/UploadShowComponent'
-AdminJS.UserComponents.UploadShowComponent = UploadShowComponent
